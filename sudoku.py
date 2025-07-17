518502e63faf694bf973992dc1471747c9afc0a1
Riley Morrison <125188389+RileyMorrison1@users.noreply.github.com>
2025-07-17 15:37:29 +0930

    Update sudoku.py
    - Added correct solution confirmation
    - Added borders to the top and bottom of the application
    - Improved the borders through being able to change a single   variable to affect them.
    - All equal numbers of the selected number are highlighted in a different colour

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 class Board:
     def __init__(self):
         # Grid is what is currently placed on the board and is not necessarily correct.
@@ -13,6 +14,7 @@ class Board:
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
         self.fixed_grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,44 +25,87 @@ class Board:
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-
-
     # The draw board function prints the board to the screen.
     def draw_board(self):
+
         # Gap is the space between each square on the board.
         gap = x // 3
-
+        x_gap, y_gap, x_gap_interval, y_gap_interval, x_pos, y_pos = 0, 0, 0, 0, 0, 0
         # Current gap is gap that is currently being used and is needed because the gap changes.
         current_gap = gap
+        value = 0
 
         # If the cursor is on the square it highlights the square.
         if self.cursor_position():
             # Records the position of the cursor into the following variables.
             x_gap, y_gap, x_gap_interval, y_gap_interval, x_pos, y_pos = self.cursor_position()
 
+            value = self.grid[y_pos - 2][x_pos - 1]
             # Prints the highlighted square onto the screen.
-            pygame.draw.rect(screen, "beige", (x_gap + 100, 0, x_gap_interval, 900))
-            pygame.draw.rect(screen, "beige", (100, y_gap, 900, y_gap_interval))
+            pygame.draw.rect(screen, "beige", (x_gap + x_offset, y_offset, x_gap_interval, 900))
+            pygame.draw.rect(screen, "beige", (x_offset, y_gap + y_offset, 900, y_gap_interval))
 
+        # Determines the x and y gap interval.
+        x_gap_interval = x // 9
 
-            pygame.draw.rect(screen, "cadetblue1", (x_gap + 100, y_gap, x_gap_interval, y_gap_interval))
+        y_gap_interval = y // 9
 
-        pygame.draw.line(screen, "black", (100, 0), (100, y), 10)
+        # Determines the font used for the numbers on the board.
+        text_font = pygame.font.SysFont("Arial", int(50))
+        win_next_colour = False
+        # Goes through each row on the board.
+        for y_square in range(9):
+            # Goes through each column on the board.
+            for x_square in range(9):
+                # Prints the number to the screen if the position on the board is not zero.
+                # Decides the colour of the square from whether it is in the fixed grid or not.
+                if self.fixed_grid[y_square][x_square] != 0:
+                    colour = "black"
+
+                else:
+                    colour = "blue"
+
+                # If the user has completed a sudoku the tiles become green. It is in a checkered pattern.
+                if (y_square == y_pos - 2) & (x_square == x_pos - 1):
+                    pygame.draw.rect(screen, "cadetblue1", (((x_square * x_gap_interval) + x_offset), ((y_square * y_gap_interval) + y_offset), (x_gap_interval), (y_gap_interval)))
+                    if win_next_colour:
+                        win_next_colour = False
+                    else:
+                        win_next_colour = True
+
+                elif self.check_win():
+                    if win_next_colour:
+                        colour = "green"
+                        win_next_colour = False
+                    else:
+                        colour = "green3"
+                        win_next_colour = True
+
+                    pygame.draw.rect(screen, colour, (((x_square * x_gap_interval) + x_offset), ((y_square * y_gap_interval) + y_offset), (x_gap_interval), (y_gap_interval)))
+                    colour = "black"
+
+                elif (self.grid[y_square][x_square] == value) & (value != 0):
+                    pygame.draw.rect(screen, "bisque2", (((x_square * x_gap_interval) + x_offset), ((y_square * y_gap_interval) + y_offset), (x_gap_interval), (y_gap_interval)))
+
+                if self.grid[y_square][x_square] != 0:
+                    self.write_text(self.grid[y_square][x_square], text_font, colour, ((x_square + 1) * x_gap_interval) + (x_gap_interval / 2.5), ((y_square + 2) * y_gap_interval) + (y_gap_interval / 3))
+
+        pygame.draw.line(screen, "black", (x_offset, y_offset), (x_offset, y + y_offset), 10)
+        pygame.draw.line(screen, "black", (x_offset, y_offset), (x + x_offset, y_offset), 10)
 
         # Prints the vertical lines between each 3x3 square on the board.
         for line in range(1, 4):
-            pygame.draw.line(screen, "black", (current_gap + 100, 0), (current_gap + 100, y), 10)
+            pygame.draw.line(screen, "black", (current_gap + x_offset, y_offset), (current_gap + x_offset, y + y_offset), 10)
             # Changes the current gap by a single gap to make the next vertical line to the right.
             current_gap += gap
 
         # Changes the gap and current gap to a new value
         gap = x // 9
-
         current_gap = gap
 
         # Prints the vertical lines between each square on the board.
         for line in range(1, 10):
-            pygame.draw.line(screen, "black", (current_gap + 100, 0), (current_gap + 100, y), 5)
+            pygame.draw.line(screen, "black", (current_gap + x_offset, y_offset), (current_gap + x_offset, y + y_offset), 5)
             # Changes the current gap by a single gap to make the next vertical line to the right.
             current_gap += gap
 
@@ -71,7 +116,7 @@ class Board:
 
         # Prints the horizontal lines between each 3x3 square on the board.
         for line in range(1, 4):
-            pygame.draw.line(screen, "black", (100, current_gap), (y + 100, current_gap), 10)
+            pygame.draw.line(screen, "black", (x_offset, current_gap + y_offset), (x + x_offset, current_gap + y_offset), 10)
             # Changes the current gap by a single gap to make the next horizontal line below the last one.
             current_gap += gap
 
@@ -82,42 +127,23 @@ class Board:
 
         # Prints the horizontal lines between each square on the board.
         for line in range(1, 10):
-            pygame.draw.line(screen, "black", (100, current_gap), (y + 100, current_gap), 2)
+            pygame.draw.line(screen, "black", (x_offset, current_gap + y_offset), (x + x_offset, current_gap + y_offset), 5)
             # Changes the current gap by a single gap to make the next horizontal line below the last one.
             current_gap += gap
 
-        # Determines the x and y gap interval.
-        x_gap_interval = x // 9
-
-        y_gap_interval = y // 9
-
-        # Determines the font used for the numbers on the board.
-        text_font = pygame.font.SysFont("Arial", int(50))
-
-        # Goes through each row on the board.
-        for y_square in range(9):
-            # Goes through each column on the board.
-            for x_square in range(9):
-                # Prints the number to the screen if the position on the board is not zero.
-                if self.fixed_grid[y_square][x_square] != 0:
-                    colour = "black"
-                else:
-                    colour = "blue"
-                if self.grid[y_square][x_square] != 0:
-                    self.write_text(self.grid[y_square][x_square], text_font, colour, ((x_square + 1) * x_gap_interval) + (x_gap_interval / 2.5), ((y_square ) * y_gap_interval) + (y_gap_interval / 3))
-
     # The cursor position function determines the position of the mouse on the board.
     def cursor_position(self):
-        x_gap = 100
+        x_gap = x_offset
+
         x_gap_interval = x // 9
         y_gap_interval = y // 9
 
         # Determines if the mouse is between the left and right hand side of the board.
-        if (pygame.mouse.get_pos()[0] > 100) & (pygame.mouse.get_pos()[0] < (x + 100)):
+        if (pygame.mouse.get_pos()[0] > x_offset) & (pygame.mouse.get_pos()[0] < (x + x_offset)):
 
             # Determines if the mouse is between the top and bottom of the board.
-            if (pygame.mouse.get_pos()[1] > 0) & (pygame.mouse.get_pos()[1] < y):
-                y_gap = 0
+            if (pygame.mouse.get_pos()[1] > y_offset) & (pygame.mouse.get_pos()[1] < (y + y_offset)):
+                y_gap = y_offset
                 found_y = False
 
                 for y_square in range(1, 11):
@@ -138,7 +164,7 @@ class Board:
                             y_pos = (y_gap // y_gap_interval)
                             x_pos = (x_gap // x_gap_interval)
 
-                            return x_gap - 100, y_gap, x_gap_interval, y_gap_interval, x_pos, y_pos
+                            return x_gap - x_offset, y_gap - y_offset, x_gap_interval, y_gap_interval, x_pos, y_pos
 
                     x_gap += x_gap_interval
 
@@ -158,11 +184,12 @@ class Board:
             # Records the position of the cursor into the following variables.
             x_gap, y_gap, x_gap_interval, y_gap_interval, x_pos, y_pos = self.cursor_position()
             # Checks if the number being placed is not on the fixed grid.
-            print(x_pos)
-            if self.fixed_grid[y_pos][x_pos - 1] == 0:
+            y_pos -= 1
+
+            if self.fixed_grid[y_pos - 1][x_pos - 1] == 0:
                 # Checks if the number is valid.
-                if (self.is_valid(y_pos, x_pos - 1, value)) | (value == 0):
-                    self.grid[y_pos][x_pos - 1] = value
+                if (self.is_valid(y_pos - 1, x_pos - 1, value)) | (value == 0):
+                    self.grid[y_pos - 1][x_pos - 1] = value
 
     # The clear function clears the board by making every value in the grid zero.
     def clear(self):
@@ -175,6 +202,7 @@ class Board:
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        
         self.fixed_grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -184,7 +212,6 @@ class Board:
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
 
     # The random function randomized the board.
     def random(self):
@@ -225,9 +252,6 @@ class Board:
                 self.grid[row][col] = 0
                 self.fixed_grid[row][col] = 0
                 remaining -= 1
-
-
-
 
     # The is valid function checks if the number being placed is valid on the specified board.
     def is_valid(self, row, col, val):
@@ -274,23 +298,34 @@ class Board:
     def show_solution(self):
         self.solve()
 
+    def check_win(self):
+        for col in range(0, 9):
+            for row in range(0, 9):
+                if self.grid[row][col] == 0:
+                    return False
+
+        return True
 
 
 running = True
 pygame.init()
 x, y = 900, 900
-screen = pygame.display.set_mode((x + 200, y + 200))
+x_offset = 100
+y_offset = 200
+screen = pygame.display.set_mode((x + (x_offset * 2), y + (y_offset * 2)))
 pygame.display.set_caption("Sudoku")
 pygame.init()
 board = Board()
+board_colour = "white"
 
 while running:
-    screen.fill("white")
+    screen.fill(board_colour)
     for event in pygame.event.get():
 
         # Detected is the program is quit.
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
         # Detected if a key is pressed down.
         if event.type == pygame.KEYDOWN:
@@ -355,9 +390,12 @@ while running:
             if event.key == pygame.K_h:
                 board.new_game(20)
 
+            # If the 'q' key is pressed the application is exited.
+            if event.key == pygame.K_q:
+                running = False
+
     # Refreshes the screen.
     board.draw_board()
     pygame.display.flip()
-
 
 pygame.quit()
